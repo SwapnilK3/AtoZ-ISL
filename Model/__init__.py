e07ec71190c0e6a255adce3072e773c71d0670c8
= <=>
2025-02-19 03:07:12 +0530

    Real time Indian Sign Language Recognition using Hand Tracking and Keypoint Classification

---
+++ Model/__init__.py
@@ -0,0 +1,1 @@
+from Model.keypoint_classifier import KeyPointClassifier
---
+++ encrypt_labels.py
@@ -0,0 +1,40 @@
+from cryptography.fernet import Fernet
+
+# Generate and print a key (do this only once, then store the key securely)
+key = Fernet.generate_key()
+print("Secret key (store this securely):", key.decode())
+
+# Encrypt labels
+labels = """A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z"""
+
+f = Fernet(key)
+encrypted_labels = f.encrypt(labels.encode('utf-8'))
+
+# Save the encrypted labels to file
+with open('model/keypoint_classifier/keypoint_classifier_label.enc', 'wb') as file:
+    file.write(encrypted_labels)
